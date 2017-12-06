6c72b3c53c7fce53173288eb970b3e22bc84352b
AJ <n.uajaykumar@gmail.com>
2017-12-06 13:19:55 +0000

    Rename the file and  added function read,write,change directory
    Rename the file and  added function read,write,change directory

---
+++ dfs_server.py
@@ -0,0 +1,164 @@
+import socket
+import threadpool
+import os
+import dfs_server_filesystem_model
+# global threadpool for server
+server_thread_pool = threadpool.ThreadPool(500)
+
+port_number = 1024
+
+ip_address = socket.gethostbyname(socket.gethostname())
+
+file_system_manager = dfs_server_filesystem_model.FileSystemManager('FileSystemDir')
+
+def create_server_socket():
+    # create socket  and initialise to localhost:8000
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('127.0.0.1', port_number)
+    print("starting up on %s port %s" % server_address)
+    # bind socket to server address and wait for incoming connections4
+    sock.bind(server_address)
+    sock.listen(1)
+
+    while True:
+        # sock.accept returns a 2 element tuple
+        connection, client_address = sock.accept()
+        # print "Connection from %s, %s\n" % connection, client_address
+        # Hand the client interaction off to a seperate thread
+        server_thread_pool.add_task(
+            start_client_interaction,
+            connection,
+            client_address
+        )
+
+
+def start_client_interaction(connection, client_address):
+    try:
+        #A client id is generated, that is associated with this client
+        client_id = file_system_manager.add_client(connection)
+        while True:
+            data = connection.recv(1024).decode()
+            split_data = seperate_input_data(data)
+            # Respond to the appropriate message
+            if data == "KILL_SERVICE":
+                kill_service(connection)
+            elif split_data[0] == "ls":
+                ls(connection, client_id, split_data)
+            elif split_data[0] == "cd":
+                cd(connection, split_data, client_id)
+            elif split_data[0] == "up":
+                up(connection, split_data, client_id)
+            elif split_data[0] == "read":
+                read(connection, split_data, client_id)
+            elif split_data[0] == "write":
+                write(connection, split_data, client_id)
+            elif split_data[0] == "delete":
+                break;
+            elif split_data[0] == "lock":
+                break;
+            elif split_data[0] == "release":
+                break;
+            elif split_data[0] == "mkdir":
+                break;
+            elif split_data[0] == "rmdir":
+                break;
+            elif split_data[0] == "pwd":
+                break;
+            elif split_data[0] == "exit":
+                break;
+            else:
+                error_response(connection, 1)
+    except:
+        error_response(connection, 0)
+        connection.close()
+
+def kill_service(connection):
+    # Kill service
+    response = "Killing Service"
+    connection.sendall("%s" % response.encode())
+    connection.close()
+    os._exit(0)
+
+def ls(connection, client_id, split_data):
+    response = ""
+    if len(split_data) == 1:
+        response = file_system_manager.list_directory_contents(client_id)
+        connection.sendall(response.encode())
+    elif len(split_data) == 2:
+        response = file_system_manager.list_directory_contents(client_id, split_data[1])
+        connection.sendall(response.encode())
+    else:
+        error_response(connection, 1)
+
+def cd(connection, split_data, client_id):
+    if len(split_data) == 2:
+        res = file_system_manager.change_directory(split_data[1], client_id)
+        response = ""
+        if res  == 0:
+            response = "changed directory to %s" % split_data[1]
+        elif res == 1:
+            response = "directory %s doesn't exist" % split_data[1]
+        connection.sendall(response.encode())
+    else:
+        error_response(connection, 1)
+
+def up(connection, split_data, client_id):
+    if len(split_data) == 1:
+        file_system_manager.move_up_directory(client_id)
+    else:
+        error_response(connection, 1)
+
+def read(connection, split_data, client_id):
+    if len(split_data) == 2:
+        response = file_system_manager.read_item(client_id, split_data[1])
+        connection.sendall(response.encode())
+    else:
+        error_response(connection, 1)
+
+def write(connection, split_data, client_id):
+    response = ""
+    if len(split_data) == 2:
+        res = file_system_manager.write_item(client_id, split_data[1], "")
+        if res == 0:
+            response = "write successfull"
+        elif res == 1:
+            response = "file locked"
+        elif res == 2:
+            response = "cannot write to a directory file"
+        connection.sendall(response.encode())
+    elif len(split_data) == 3:
+        res = file_system_manager.write_item(client_id, split_data[1], split_data[2])
+        if res == 0:
+            response = "write successfull"
+        elif res == 1:
+            response = "file locked"
+        elif res == 2:
+            response = "cannot write to a directory file"
+        connection.sendall(response.encode())
+    else:
+        error_response(connection, 1)
+
+
+def exit(connection, split_data, client_id):
+    if len(split_data) == 1:
+        file_system_manager.disconnect_client(connection, client_id)
+    else:
+        error_response(connection, 1)
+
+def error_response(connection, error_code):
+    response = ""
+    if error_code == 0:
+        response = "server error"
+    if error_code == 1:
+        response = "unrecognised command"
+    connection.sendall(response.encode())
+
+#Function to split reveived data strings into its component elements
+def seperate_input_data(input_data):
+    seperated_data = input_data.split('////')
+    return seperated_data
+
+if __name__ == '__main__':
+    create_server_socket()
+    # wait for threads to complete
+    server_thread_pool.wait_completion()
